ca28c1eb983a92329952ad29a369a30cfb14de68
Ahmed Khalil <progeng_ahmed_khalil@outlook.com>
2021-04-09 18:54:34 +0200

    Add Singleton

---
+++ 2. The Singleton Pattern __Creational/DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Creating the instance of singleton");
+            var singleton1 = Singleton.Instance;
+            var singleton2 = Singleton.Instance;
+            if (singleton1 == singleton2)
+            {
+                System.Console.WriteLine("The Instances are the same");
+            }
+            else
+            {
+                System.Console.WriteLine("The instances are different");
+            }
+        }
+    }
+}
+
--- 2. The Singleton Pattern __Creational/README.md
+++ 2. The Singleton Pattern __Creational/README.md
@@ -1,12 +1,5 @@
 # In the Name of ALLAH
-# Software-Design-Pattern
----
-- In software engineering, a __design pattern__ is a general repeatable solution to a commonly occurring problem in software design.
 
+# Singleton Design Pattern
 
-
-- A design pattern isn't a finished design that can be transformed directly into code.
-
-
-- It is a description or template for how to solve a problem that can be used in many different situations.
-
+- Ensures a class should hav only one instance, and provide a global access to it.
